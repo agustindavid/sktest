1bcf155750ab8f1b13d553e173824a9924727b50
unknown <agustindavid@gmail.com>
2022-02-11 07:30:12 -0600

    layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/fb10955868.js" crossorigin="anonymous"></script>
+</svelte:head>
+
+aqui va la navegacion
+
+    <slot></slot>
+
+<footer class=" ">
+  <div class="flex flex-wrap justify-center text-center py-8 text-white bg-fuga-pink w-full">
+    <h4>Fuga Ciclismo Inteligente</h4>
+  </div>
+</footer>
