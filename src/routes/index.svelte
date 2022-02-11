05588e3a7e9854d3cde065fc8d0a12c7bc61d8ae
unknown <agustindavid@gmail.com>
2022-02-11 07:19:38 -0600

    hi24454

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,9 +6,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<form name="test" method="post" netlify netlify-honeypot="bot-field">
+<form name="test" method="post" netlify>
     <input type="hidden" name="form-name" value="test" />
-    <input type="text" name="bot-field" />
     <p>
         <label>Your Name: <input type="text" name="name" /></label>
     </p>
