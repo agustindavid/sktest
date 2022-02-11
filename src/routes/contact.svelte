bf7fe407fef6383f5c7366265a4c3f58b9b0c973
unknown <agustindavid@gmail.com>
2022-02-11 07:42:06 -0600

    layout2

---
+++ src/routes/contact.svelte
@@ -0,0 +1,23 @@
+<script context="module">
+    export const prerender = true;
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form name="contacto" method="post" netlify netlify-honeypot="bot-field">
+    <input type="text" name="bot-field" />
+    <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+    </p>
+    <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+    </p>
+    <p>
+        <label>Message: <textarea name="message" /></label>
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+</form>
